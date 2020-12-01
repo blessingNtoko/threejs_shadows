1f32ed147202cd185398a4d0fa5fe1d71916ba19
Blessing <bntoko@gmail.com>
2020-12-01 09:55:07 +0200

    basic setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'threejsShadows';
+export class AppComponent implements OnInit{
+
+  public scene = new THREE.Scene();
+  public renderer = new THREE.WebGLRenderer({
+    antialias: true
+  });
+  public camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, .1, 1000);
+  public controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+  ngOnInit() {
+    this.init();
+  }
+
+  private init() {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+
+    this.controls.update();
+    this.camera.position.set(0, 10, 0);
+  }
 }
